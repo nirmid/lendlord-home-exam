bccc450585f6c04aa0e99b3ddcc81241eda6a9f8
Lendlord <67461096+lendlord-io@users.noreply.github.com>
2024-07-16 10:28:07 +0300

    Update headers.js

--- frontend/src/shared/headers.js
+++ frontend/src/shared/headers.js
@@ -1,11 +1,4 @@
-export const getHeaders = () => {
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-
-  headers.append('Access-Control-Allow-Origin', 'http://localhost:3001');
-  headers.append('Access-Control-Allow-Credentials', 'true');
-
-  headers.append('GET', 'POST', 'OPTIONS');
-  return headers
-}
+export const getHeaders = () => ({
+  'Content-Type': 'application/json',
+  'Accept': 'application/json'
+})
